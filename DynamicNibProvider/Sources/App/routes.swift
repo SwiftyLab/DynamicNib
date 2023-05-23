96258fac055950d42e2ea5744aa4db677c7a2618
soumyamahunt <soumya.mahunt@gmail.com>
2023-05-24 02:05:25 +0530

    feat: add javascript bridged controller

--- DynamicNibProvider/Sources/App/routes.swift
+++ DynamicNibProvider/Sources/App/routes.swift
@@ -10,15 +10,27 @@ func routes(_ app: Application) throws {
         "Hello, world!"
     }
     
-    let group = app.grouped("nibs")
+    let nibGroup = app.grouped("nibs")
     let resourceURL = Bundle.module.resourceURL!
-    group.get { req async throws -> [String] in
+    nibGroup.get { req async throws -> [String] in
         let manager = FileManager.default
         return try manager.contentsOfDirectory(atPath: resourceURL.path)
             .filter { $0.hasSuffix(".nib") }
     }
-    group.get(":name") { req async throws in
+    nibGroup.get(":name") { req async throws in
         let nibURL = resourceURL.appending(path: "\(req.parameters.get("name")!).nib")
         return req.fileio.streamFile(at: nibURL.path)
+    }
+
+    let jsGroup = app.grouped("js")
+    let jsResourceURL = Bundle.module.resourceURL!.appending(path: "Js")
+    jsGroup.get { req async throws -> [String] in
+        let manager = FileManager.default
+        return try manager.contentsOfDirectory(atPath: jsResourceURL.path)
+            .filter { $0.hasSuffix(".js") }
+    }
+    jsGroup.get(":name") { req async throws in
+        let jsURL = jsResourceURL.appending(path: "\(req.parameters.get("name")!).js")
+        return req.fileio.streamFile(at: jsURL.path)
     }
 }
